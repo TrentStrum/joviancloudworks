db06b4b7a4094d451053d8678f5887f4acb15849
tstrum <trent.strum@gmail.com>
2025-01-26 13:31:28 -0800

    clean up

--- hooks/auth/useAuthState.ts
+++
@@ -1,8 +0,0 @@
-import { useEffect, useState } from 'react';
-import { User } from '@/types/user.types';
-
-export function useAuthState() {
-	const [user, setUser] = useState<User | null>(null);
-
-	return { user };
-}
