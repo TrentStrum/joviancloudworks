e650690aa9d006548428288b7e219cef5d2182ae
tstrum <trent.strum@gmail.com>
2025-01-25 19:28:11 -0800

    Update skeletons index exports to use BlogPostSkeleton

--- components/skeletons/index.tsx
+++ components/skeletons/index.tsx
@@ -1,6 +1,4 @@
 'use client';
 
+export { BlogPostSkeleton } from './BlogPostSkeleton';
 export { SkeletonOne } from './skeleton-one';
-export { SkeletonTwo } from './skeleton-two';
-export { SkeletonThree } from './skeleton-three';
-export { SkeletonFour } from './skeleton-four';
